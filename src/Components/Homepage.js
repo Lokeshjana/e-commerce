be68f58504602522bf5d003077672a1656af261a
Ramesh Inumula <ramesh@unosimple.com>
2021-09-19 15:38:30 +0530

    first commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from '../Common/Header'
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
